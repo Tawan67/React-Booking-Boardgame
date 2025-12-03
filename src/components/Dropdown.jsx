3787a9404521c73f5c0e74328d7fddef31dd86a2
Tawan67 <tawankhuntha@gmail.com>
2025-12-03 13:31:27 +0700

    try to do dropdown

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,17 @@
+import React, { useRef, useState } from 'react'
+import './Dropdown.css'
+const Dropdown=() => {
+  
+    return(
+<div className='dropDown'>
+<ul>
+    <li>Theme</li>
+    <li>Player Count</li>
+    <li>Collected</li>
+    <li>Game Type</li>
+</ul>
+</div>
+    )
+}
+
+export default Dropdown
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
+import Dropdown from "../components/Dropdown";
 function HomePage(){
     return(
         <>
+        <Dropdown/>
         <div>
         <search><input type="text" placeholder="seacrh"/></search>
-        <div className="tag">tag</div>
+        <ul className="tag">
+            <li>
+                <a href="#">Tag</a>
+                <ul>
+                    <li>Theme</li>
+                    <li>Player Count:</li>
+                    <li>Collection Management</li>
+                    <li>Game Type:</li>
+                    <li>Player Status:</li>
+                </ul>
+            </li>
+        </ul>
         </div>
         
         
